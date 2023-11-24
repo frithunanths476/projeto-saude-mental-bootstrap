cf61161a37c36a04868a3cfe38b91e902c661d40
frithunanths476 <124930953+frithunanths476@users.noreply.github.com>
2023-11-24 14:03:50 -0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -32,3 +32,29 @@ btnDalto.addEventListener("click", function(){
         body.className = "dalto"
     }
 })
+
+//-----------------------------------------------
+
+var btnSN = document.getElementById("logo_sobre")
+
+function sobre(){
+    Swal.fire(
+        'Quem somos?',
+        'Somos o maior grupo privado de atendimento psiquiátrico do Brasil, com 4 unidades de internação, 350 leitos, ambulatório, hospital dia.',
+        'info'
+    )
+}
+
+btnSN.addEventListener("click", sobre)
+
+//-----------------------------------------------
+
+var btnLocal = document.getElementById("localizacao")
+
+function endereco(){
+    Swal.fire(
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d464.64365392587024!2d-50.33848815575477!3d-21.30511886213329!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1700833690794!5m2!1spt-BR!2sbr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+    )
+}
+
+btnLocal.addEventListener("click", endereco)
